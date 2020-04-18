1cd87628a4dc3d2e063428564e7e8c334f15e27a
tomsaunders98 <tomandthenews@gmail.com>
2020-04-18 22:25:21 +0100

    Graphs

--- DataViz.r
+++ DataViz.r
@@ -5,7 +5,8 @@ library(gcookbook)
 testnumbers <- read_csv("Testnumbers.csv")
 
 #Number of countries to graph:
-num = 10
+num = 20
+countries <- list( "UK")
 
 # Descriptive Sample Explaners
 for(i in 1:nrow(testnumbers)){
@@ -14,7 +15,7 @@ for(i in 1:nrow(testnumbers)){
     temp = "Number of People Tested"
   }
   if (val == "Unclear"){
-    temp = "Unclear Measuremt (see notes)"
+    temp = "Measurement Not Specified"
   }
   if (val == "Samples"){
     temp = "Number of Laboratory Samples Analysed"
@@ -31,22 +32,25 @@ testnumbers$max <- apply(testnumbers[7:(ncol(testnumbers)-1)], 1, max,na.rm=TRUE
 
 # Develop Bar Chart Columns
 testnumbersBar <- testnumbers %>% 
+  #filter(Country %in% countries) %>%
   mutate(
     TestsPerCap = as.double(max)/as.double(Population),
     TestsPerMil = as.double(max)/1000000
   ) %>%
-  arrange(desc(Population)) %>%
+  arrange(desc(max)) %>%
   head(n=num)
+  
 
 update_geom_font_defaults(font_rc_light)
 ## Tests Per Capita (bar)
+maxnum = max(testnumbersBar$TestsPerCap, na.rm=TRUE) + 0.005
 
 plotPC <- ggplot(data = testnumbersBar, aes(x = reorder(CountryNames, TestsPerCap), y = TestsPerCap, fill = DataType)) +
   geom_bar(stat = "identity") +
   geom_text(aes(label=datemax, colour = DataType),  vjust=0.5, hjust=0, show.legend = FALSE)
 
 plotPC + 
-  scale_y_continuous(limits=c(0,0.015)) +
+  scale_y_continuous(limits=c(0,maxnum)) +
   xlab("Countries ")+
   ylab("Tests Per Capita") +
   ggtitle("COVID-19 Tests Completed Per Capita") +
@@ -65,7 +69,7 @@ plotPM <- ggplot(data = testnumbersBar, aes(x = reorder(CountryNames, TestsPerMi
   geom_text(aes(label=datemax, colour = DataType),  vjust=0.5, hjust=0, show.legend = FALSE)
 
 plotPM + 
-  scale_y_continuous(limits=c(0,1.9)) +
+  scale_y_continuous(limits=c(0,4.5)) +
   xlab("Countries ")+
   ylab("Tests Per Million") +
   ggtitle("COVID-19 Tests Completed Per Million") +
@@ -75,16 +79,19 @@ plotPM +
   scale_fill_ipsum() +
   theme_ipsum_rc(grid="X", axis="X")
 
-
+num = 10
 ## Line Graph, Tests Per Million
 # Remove Max as not needed for now
 testnumbers$max <- NULL
 
 #develop line columns
-testnumbers[7:(ncol(testnumbers)-1)] <- testnumbers[7:(ncol(testnumbers)-1)]/1000000 
+testnumbers[7:(ncol(testnumbers)-1)] <- testnumbers[7:(ncol(testnumbers)-1)]/1000000#testnumbers$Population
+
+
 linedata <- testnumbers %>%
   arrange(desc(Population)) %>%
   head(n=num) %>%
+  #filter(Country %in% countries) %>%
   pivot_longer(7:(ncol(testnumbers)-1), names_to="dates", values_to="Total") %>%
   drop_na(Total) %>%
   mutate(dates = as.Date(dates, "%d/%m/%Y")) %>%
@@ -94,21 +101,35 @@ linedata <- testnumbers %>%
 
 # Develop Line Graph
 
-LinePM <- ggplot(data = linedata, aes(x = dates, y = Total, group = CountryNames, color=CountryNames)) +
-  geom_line() +
+LinePM <- ggplot(data = linedata, aes(x = dates, y = Total, group = CountryNames,color=CountryNames)) +
   geom_point(aes(shape=DataType)) +
+  geom_line() +
   geom_label_repel(aes(label = label),
-                 nudge_x=0.01,
+                 nudge_x=1,
                  na.rm = TRUE,
                  nudge_y=0.01,
                  min.segment.length = Inf)
 
+
 LinePM + 
+  #scale_y_continuous(limits=c(0,1.2)) +
   xlab("Dates")+
   ylab("Tests Per Million") +
   ggtitle("COVID-19 Tests Completed Per Million") +
   labs(shape = "Data Collected As:") +
   guides(color=FALSE) +
-  #scale_color_ipsum() +
+  scale_color_ipsum() +
   theme_ipsum_rc(grid="XY", axis="xy")
 
+
+
+#geom_smooth( method='lm') +
+
+ # theme(legend.position="none")
+#labs(caption="What does 100,000 tests a day look like ?") +
+
+# annotate("segment", x = as.Date("2020-03-30"), y = 0.125, xend = as.Date("2020-04-08"), yend = 1.025,
+#    colour = "#8fd175", alpha = .8, size=1)
+# annotate("text", x = as.Date("2020-04-03"), y = 0.75, label = "100,000 Tests Per Day", colour = "#8fd175", size=6)
+#geom_abline(intercept = 37, slope = 0.1, color="red", 
+#      linetype="dashed", size=1.5)+
